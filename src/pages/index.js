1936d4e4542fd247197c3359f6815217617c57af
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2024-11-28 19:10:38 -0500

    API1

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
         { image_file: image, size: "auto" },
         {
           headers: {
-            "X-Api-Key": process.env.NEXT_PUBLIC_API_1,
+            "X-Api-Key": API1,
             "Content-Type": "multipart/form-data"
           },
           responseType: "blob"
